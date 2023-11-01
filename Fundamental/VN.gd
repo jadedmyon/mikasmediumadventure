8bf655956074a66a50ea4677305a120b3ede8fca
jadedmyon <jadedmyon@gmail.com>
2023-11-02 04:14:13 +0600

    VN scenes can now be defined in a text file

--- Fundamental/VN.gd
+++ Fundamental/VN.gd
@@ -12,9 +12,10 @@ var currentscene = [
 	["print","Why is everybody so mean to me"],
 	["print","im so mad im going to drink juice and fuck my wife i feel so mad"],
 	["speaker","Normal person"],
+	["speakercolor",0xc3f9fb],
 	["print","thats paint thinner"],
+	["addscene","testvn"],
 	
-
 ]
 var sceneindex := 0 #currentscene list index
 
@@ -22,8 +23,8 @@ var sceneindex := 0 #currentscene list index
 
 
 var speakercolors:Dictionary = {
-	"Mika" : 0xDDBFF8
-	
+	"Debut Mika" : 0xDDBFF8,
+	"Mika" : 0xDDBFF8,
 	}
 
 
@@ -38,20 +39,27 @@ func processline():
 
 	var instantcontinue = true #used for speakers. This will also mean an unknown command will just be skipped
 
-	if commandname == "print":
+	if commandname in ["print","p"]:
 		printspeech(command[1])
 		instantcontinue = false 
-	
-	if commandname == "speaker":
+
+	if commandname in ["speaker","s"]:
 		speakername(command[1])
 		#default speaker color
 		if command[1] in speakercolors:
 			$speakerbox/label.add_theme_color_override("font_color", Color.hex(speakercolors[command[1]]))
 		else:
 			$speakerbox/label.add_theme_color_override("font_color", Color(1,1,1,1)) 
+	if commandname in ["speakertext","st"]:
+		speakername(command[1])
+	if commandname in ["speakercolor","sc"]:
+		speakercolor(command[1])
+	if commandname in ["addscene"]:
+		addscene(command[1])
 	
 	
-	##Continues scene. Needs to be a separate function for the same frame loop to work 
+	
+	##Continues scene. Needs to be a separate function for the same frame loop to work(?)
 	continuescene(instantcontinue)
 
 
@@ -68,14 +76,43 @@ func speakername(text:String):
 	$speakerbox/label.text = text 
 
 func speakercolor(color): #not sure what type 
-	$speakerbox/label.add_theme_color_override("font_color", Color.hex(color)) 
+	$speakerbox/label.add_theme_color_override("font_color", Color.hex(int(color))) 
+
+
+
+
+
+
+func addscene(scenename:String):
+
+	
+	var loadfile = FileAccess.open('res://VNScenes/' + scenename + '.mikascene', FileAccess.READ)
+
+	var splitperline = loadfile.get_as_text().split("\n")
+	for line in splitperline:
+		var linesplit:Array = line.split(";")
+		var commandname:String = linesplit[0]
+		if len(linesplit) == 1: #regular print line
+			currentscene.append(["print",linesplit[0]])
+		else:
+			currentscene.append(linesplit)
+	
+	loadfile.close() 
+
+func loadscene(scenename:String):
+	currentscene = []
+	addscene(scenename)
+	processline()
 
 
 
 func _ready():
-	pass 
+	
+	processline()
+	loadscene("testvn")
 
 
 func _process(delta):
 	if Input.is_action_just_pressed("A"):
 		processline()
+		
